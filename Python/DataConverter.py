eadd3afd3a7f967b1070f8ef99ec9ab184c80fab
Corne <therealcorne@gmail.com>
2022-07-11 14:56:22 +0200

    Date comparison fixed + dest folder added

--- Python/DataConverter.py
+++ Python/DataConverter.py
@@ -4,8 +4,9 @@ from google.cloud import storage
 import os
 from dateutil import parser,tz
 from datetime import datetime,timedelta
-
+import tempfile
 import csv
+TEMP_DIR = tempfile.gettempdir()
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'service_account.json'
 
@@ -83,71 +84,86 @@ def list_buckets():
     for bucket in buckets:
         print(bucket.name)
 
-def create_bucket(mqtt_data):
-    FILE_PREFIX = "EnergyData"
+def save_to_cloud(mqtt_data):
+    FILE_PREFIX = "Energy"
     BUCKET_NAME = "mqtt_data_1"
-    FILE_TYPE = ".txt"
-
+    FILE_TYPE = "txt"
+    TEMP_DIR = tempfile.gettempdir()
+    DEST_FOLDER = "Braby"
 
     storage_client = storage.Client()                   # get client session
     bucket = storage_client.get_bucket(BUCKET_NAME)     # Get bucket
-
     # check to see where new data should be written to by
     # getting the date from the filename of the last file and check if now's date matches the file date
-
-    blobs = list_blobs(BUCKET_NAME)
-
+    list_of_filenames = list_blobs(BUCKET_NAME)
     TIMEZONE = "GMT+2"
     todays_date = datetime.now(tz=tz.tzstr(TIMEZONE))
-    # todays_date = todays_date + timedelta(days=1)
-
+    # todays_date += timedelta(days=1)                  # used for testing
     # first list all the blobs
-    if not FILE_PREFIX in blobs:            # if there are no files in the bucket with FILE_PREFIX, then create a new file
-
+    file_exists = False
+    list_of_target_filenames = []
+    for filename in list_of_filenames:
+        if FILE_PREFIX in filename:
+            list_of_target_filenames.append(filename)
+            file_exists = True
+    print(list_of_target_filenames)
+    del list_of_filenames
+    if not file_exists:
         todays_date_string = todays_date.strftime("%Y-%m-%d")
 
-        new_filename = f'/tmp/{FILE_PREFIX}_{todays_date_string}.{FILE_TYPE}'  # build filename string
-        print(f"No existing files have been found. Creating new file: {new_filename}")
-        with open(new_filename, "w") as file:  # create new file with todays date
-            file.writelines(mqtt_data)
-        upload_filename = new_filename
+        filename = f'{FILE_PREFIX}_{todays_date_string}.{FILE_TYPE}'  # build filename string
+        file_path = f"{TEMP_DIR}/{filename}"
+        print(f"No existing files have been found. Creating new file: {file_path}")
+        with open(file_path, "w") as file:  # create new file with todays date
+            file.write(mqtt_data)
+        upload_filename = f"{DEST_FOLDER}/{filename}"
 
-    elif FILE_PREFIX in blobs:
-        print("Looking for existing file")
-        last_blob_filename = blobs[-1]                      # get last file
+    elif file_exists:
+        print("Existing files exist. Looking for most recent file")
+        last_blob_filename = list_of_target_filenames[-1]                      # get last file
         # last_blob_filename=  "EnergyData_2022-06-11"
-        last_blob_date = last_blob_filename.split("_")[1]   # extract the data string
+        last_blob_date = last_blob_filename.split("_")[1].split(".")[0]   # extract the data string
         last_date = parser.parse(last_blob_date)            # parse string into date
+        file_path = f"{TEMP_DIR}/{last_blob_filename}"
 
 
         # if days does not match, create new file with current date to it, add the data to that file and upload it
-        if last_date.day < todays_date.day:             # check if today's day is the same as the files date
+        if datetime.date(last_date) < datetime.date(todays_date)  :             # check if today's day is the same as the files date
             # days are different, this should only trigger when first implemented and at midnight
 
             todays_date_string = todays_date.strftime("%Y-%m-%d")
 
-            new_filename = f'/tmp/{FILE_PREFIX}_{todays_date_string}.{FILE_TYPE}'    # build filename string
-            print(f"Writing to file: {new_filename}")
-            with open(new_filename,"w") as file:                        # create new file with todays date
-                file.writelines(mqtt_data)                             # write data to file
+            filename = f'{FILE_PREFIX}_{todays_date_string}.{FILE_TYPE}'    # build filename string
+            file_path = f"{TEMP_DIR}/{filename}"
+            print(f"Most recent file does not match today's date. Creating a file for new day. Writing to file: {file_path}")
+            with open(file_path,"w") as file:                        # create new file with todays date
+                file.write(mqtt_data)                                # write data to file
 
-            upload_filename = new_filename
+            upload_filename = f"{DEST_FOLDER}/{filename}"
 
 
         # if it matches, download that file, append new data to it and re-upload the file
-        elif last_date.day == todays_date.day:
+        elif datetime.date(last_date) == datetime.date(todays_date):
 
-            last_day_blob_ = bucket.get_blob(last_blob_filename)
-            last_day_blob_.download_to_filename(f"/tmp/{last_blob_filename}")
-            filename = f'/tmp/{last_blob_filename}.{FILE_TYPE}'
-            print(f"Dates matched. Wrting to existing file: {filename}")
-            with open(filename, 'a') as file:
-                file.writelines(mqtt_data)
-            upload_filename = f"{last_blob_filename}.{FILE_TYPE}"
+            last_day_blob = bucket.get_blob(last_blob_filename)
+            print(f"Downloading: {last_blob_filename}")
+            temp_name = f"{TEMP_DIR}/{last_blob_filename.split('/')[1]}"
+            last_day_blob.download_to_filename(temp_name)
+            # filename = f'{last_blob_filename}'
+            # file_path = f"{TEMP_DIR}/{filename}"
+            print(f"Most recent file matches today's date. Writing to existing file: {file_path}")
+            with open(temp_name, 'a') as file:
+                file.write('\n')
+                file.write(mqtt_data)
+            upload_filename = last_blob_filename
+            file_path = temp_name
+
+        else:
+            raise ("Date comparison error")
 
     print(f"Uploading file: {upload_filename}")
-    blob = bucket.blob(f"/tmp/{upload_filename}")              # convert file to blob
-    blob.upload_from_filename(upload_filename)       # upload file to bucket
+    blob = bucket.blob(f"{upload_filename}")              # convert file to blob
+    blob.upload_from_filename(file_path)       # upload file to bucket
 
 
 
@@ -159,9 +175,10 @@ def create_bucket(mqtt_data):
 
 if __name__ == "__main__":
     # blobs = list_blobs("mqtt_data_1")
+    # print(blobs)
     # # Get blobs in specific subirectory
     # lastFileName = blobs[-2]
 
     data = "227.702 ;50 ;2977.57 ;2977.69 ;552.393 ;1654772376"
-    create_bucket(data)
+    save_to_cloud(data)
 
